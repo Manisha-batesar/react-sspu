66ee7b538897e3be5db656325e7d5b95bb358b1f
Manisha-batesar <manubatesar@gmail.com>
2025-03-04 18:08:16 +0530

    feat: react dom setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 
-const rootDiv = document.getElementById("root");
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>Hello Manisha</h1>);
