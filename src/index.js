b7202d851c301d9a6065890b364cb8b9203061c6
Manisha-batesar <manubatesar@gmail.com>
2025-03-04 18:28:17 +0530

    feat: setup webpack html plugin

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { createRoot } from "react-dom/client";
 
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello Manisha</h1>);
+root.render(<h1>Hello Manisha and rajesh</h1>);
