1802aef29e88adf412211300e45eced194fc551d
Manisha-batesar <manubatesar@gmail.com>
2025-03-04 18:22:42 +0530

    feat: setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path:path.resolve(__dirname, "dist"),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
 }
