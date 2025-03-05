ac5bb0ca8d700c935fc1c6954a89a9e67fdb1576
Manisha-batesar <manubatesar@gmail.com>
2025-03-05 18:41:27 +0530

    feat:add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,10 @@ module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path:path.resolve(__dirname, "dist"),
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
 }
