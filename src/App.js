b7af78d6d2fb814c85554b16d83738938117f725
Manisha-batesar <manubatesar@gmail.com>
2025-03-06 10:52:53 +0530

    feat: add js files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './components/Counter'
+import ToDoList from './components/ToDoList'
+
+const App = () => {
+  return (
+    <> 
+    <Counter />
+    <ToDoList />
+    </>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const root = document.getElementById("root");
 const reactRoot = createRoot(root);
 
-reactRoot.render(<h1>Hello world</h1>)
+reactRoot.render(<App />)
