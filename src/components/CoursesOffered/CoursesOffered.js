af29c37d1451a4020b6bb27a8f16db26213c9843
Manisha-batesar <manubatesar@gmail.com>
2025-03-26 17:51:02 +0530

    feat: implement CoursesOffered component with styling and content

--- src/components/CoursesOffered/CoursesOffered.js
+++ src/components/CoursesOffered/CoursesOffered.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import './CoursesOffered.css'
+import './CoursesOffered.css';
+import studentSalon from '../../assets/image3.png';
 
-const CouesesOffered = () => {
+const CoursesOffered = () => {
   return (
-    <div>CouesesOffered</div>
+
+    <div className='Container'>
+
+      <h2>Courses Offered</h2>
+
+      <div className='courseContainer'>
+
+        <div className='courseContent'>
+
+          <div className='offeredCourses'>
+            <span>BSc. Beauty & Wellness</span>
+            <span>BSc.Nutritional Sciences & Diabetics</span>
+            <span>MSc.Nutritional Sciences & Diabetics</span>
+          </div>
+          <div className='skillContent'>
+            <p>Symbiosis Skills & Professional University, in collaboration with
+              Enrich Salons & Academy, brings you a world-class Training Academy on campus. Designed as
+              per CIDESCO standards, our state-of-the-art labs and live work environment offer hands-on
+              learning in Beauty, Wellness, and Aesthetics.</p>
+          </div>
+
+        </div>
+        <div className='courseImage'>
+          <img src={studentSalon} alt='courseImage' />
+        </div>
+
+      </div>
+    </div>
+
   )
+
 }
 
-export default CouesesOffered
+export default CoursesOffered
