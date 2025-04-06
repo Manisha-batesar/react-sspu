30fbe7eac450024886eda435a027d761e1769016
Manisha-batesar <manubatesar@gmail.com>
2025-04-06 21:36:37 +0530

    feat: enhance ApplyNow and CampusTour components with new layout and styling; add footer content and improve responsiveness

--- src/components/ApplyNow/ApplyNow.js
+++ src/components/ApplyNow/ApplyNow.js
@@ -1,16 +1,74 @@
 import React from 'react'
 import './ApplyNow.css'
 import formImage from '../../assets/formImage.jpg'
+import logoImage from '../../assets/icon1.png'
 
 const ApplyNow = () => {
   return (
+
     <div className='ApplyNowContainer'>
-      <div className='formContent'>
-        <img src={formImage} alt='formImage'/>
-        <p>dfhdff</p>
+      <img src={formImage} alt='formImage' className='footer-bg-image' />
+      <div className='footer-backdrop' />
+
+      <div className='footer-content'>
+        <div className='enquiryDivision'>
+          <div className='footer-logo'>
+            <img src={logoImage} alt='footer logo sspu' />
+          </div>
+          <p>sspu@gmail.com</p>
+          <div className='general-inquiry'>
+            <h4>For general enquiry</h4>
+            <ul>
+              <li>02027187768</li>
+              <li>7796638369</li>
+              <li>7796695604</li>
+            </ul>
+          </div>
+
+
+          <div className='sspu-Directions'>
+            <h4>SSPU Directions</h4>
+            <p>SSPU Kiwale, adjoining Pune-Mumbai <br />Expressway, Pune- 412 101</p>
+          </div>
+        </div>
+
+
+
+        <div className='contentDivision'>
+          <div className='contact-us'>
+            <h4>Contact</h4>
+            <p>For Reception</p>
+            <ul>
+              <li>02027187768</li>
+            </ul>
+            <h6>For New Admission Enquiry</h6>
+            <ul>
+              <li>02027187768</li>
+              <li>7796638369</li>
+              <li>7796695604</li>
+            </ul>
+          </div>
+        </div>
+
+
+
+        <div className='psychologicalDivision'>
+          <div className='counsellor-detailss'>
+            <h4>PsyCHological Counsellor Details</h4>
+            <p>Timings: 9 am to 5 pm;
+              Monday, Wednesday and Friday</p>
+            <h6>Location: Health Center, SSPU Kiwale</h6>
+            <p>Campus Tour</p>
+            <p>See on Map</p>
+          </div>
+        </div>
+       
       </div>
 
+      <p className='copyRight'>&copy; 2025 My Website. All rights reserved.</p>
     </div>
+   
+     
   )
 }
 
--- src/components/CampusTour/CampusTour.js
+++ src/components/CampusTour/CampusTour.js
@@ -4,26 +4,23 @@ import campusImage from '../../assets/image4.png';
 
 const CampusTour = () => {
   return (
-    <div className='container'>
-     <div className='campusimage'>
-
-      <img src={campusImage} alt='campus'/>
-      <div className='campusContent'>
-        <div className='leftContent'>
-        <p>A breathtaking campus you can’t resist!</p>
-        <button type='button'>Book a Campus Tour<span>→</span></button>
-        </div>
-        <div className='rightContent'>
-          <div className='content'>
-            <p>100<br/><span>Stats</span></p><hr/>
-            <p>100+<br/><span>Stats</span></p><hr/>
-            <p>100<br/><span>Stats</span></p>
+    <>
+      <div className='container'>
+        <div className='campusimage'>
+          <img src={campusImage} alt='campus' />
+          <div className='campus-tour-content'>
+            <p>A breathtaking campus you can’t resist!</p>
+            <button type='button'>Book a Campus Tour<span>→</span></button>
           </div>
         </div>
       </div>
-     </div>
+      <div className='campus-stats'>
+        <p>100<br /><span>Stats</span></p><hr />
+        <p>100+<br /><span>Stats</span></p><hr />
+        <p>100<br /><span>Stats</span></p>
+      </div>
+    </>
 
-    </div>
   )
 }
 
